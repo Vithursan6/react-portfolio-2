f504fa72275dbb38ca316992b3cb67f7d548a4b7
vithursan <93847590+Vithursan6@users.noreply.github.com>
2022-10-21 18:54:43 -0400

    added some extra devicons to skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,8 +10,9 @@ function Skills() {
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="The logo icon for HTML 5" title="HTML 5"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="The logo icon for CSS3" title="CSS 3"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons//javascript/javascript-original.svg" alt="The logo icon for Javascript" title="js-1"/>
-                        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="The logo icon for GitHub" title="GitHub"/>
+                        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="The logo icon for NodeJS" title="Node JS"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="The logo icon for NodeJS" title="Node JS"/>
+                        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="The logo icon for GitHub" title="GitHub"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="The logo icon for Mongodb" title="mongo-i"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" alt="The logo icon for MySQL" title="mysql-i"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="The logo icon for NPM" title="NPM"/>
@@ -30,6 +31,7 @@ function Skills() {
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt="The logo icon for Sass" title="sass-I"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg" alt="The logo icon for Nginx" title="nginx-i"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original-wordmark.svg" alt="The logo icon for Gitlab" title="gitlab-i"/>
+                        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/digitalocean/digitalocean-original-wordmark.svg" alt="The logo icon for Gitlab" title="gitlab-i"/>
                         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original-wordmark.svg" alt="The logo icon for Slack" title="slack-i"/>
 
                     </div>
