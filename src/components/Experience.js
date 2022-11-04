15b18e5a3e8109cb0c97c4309f6f1e272a291886
vithursan <93847590+Vithursan6@users.noreply.github.com>
2022-11-04 16:55:35 -0400

    edited experience info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,39 +10,62 @@ const Experience = () => {
             <div className='timeline-block timeline-block-right'>
                 <div className='marker'></div>    
                     <div className='animate__animated animate__lightSpeedInRight animate__slow timeline-content'>
-                        <h3>2014-2015</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        A nulla animi error minus unde ex! Quidem assumenda ut alias
-                        nulla aperiam a nam placeat, ipsa reprehenderit sequi dolores
-                        ullam iusto.
-                        </p>
+                        <h3>2020-2022</h3>
+                        <h4>Warehouse Lead/Shipper/Receiver - Nustef Baking</h4>
+                        <ul>
+                            <li>Trained employees on equipment operation and safety protocols.</li>
+                            <li>Oversaw daily operations and shipping and handling processes. </li>
+                            <li>Oversaw warehouse staff and maintained efficiency in fast-paced environment.</li>
+                            <li>Recorded information, shortages and discrepancies to keep records current and accurate.</li>
+                            <li>Managed day-to-day operations of warehouse, freight and parcel shipments, returns and transfers.</li>
+                            <li>Loaded, unloaded and sorted cargo as part of accurate and efficient weekly shipments.</li>
+                            <li>Promoted safety policies and practices among personnel, enforcing appropriate handling and use of equipment and products</li>
+                            <li>Collaborated with management to implement new initiatives and policies and achieve benchmarks for production, quality and safety</li>
+                            <li>Used hand-held scanners and physical logs to accurately track item movements.</li>
+                            <li>Double checked records, daily reports and inventory transactions to identify and correct variances.</li>
+                            <li> Listened closely during team meetings to gain complete understanding of duties required for each shift, completing priority tasks first.</li>
+                            <li>Delivered updates on anticipated task completion times.</li>
+                            <li>Performed inventories and stocked new equipment and goods. </li>
+                            <li>Acted as liaison between company departments and warehouse teams.</li>
+                            <li>Prevented load shifting or damage by using bracing and strapping techniques</li>
+                        </ul>
                     </div>
                 </div>
 
             <div className='timeline-block timeline-block-left'>
                 <div className='marker'></div>
                     <div className='animate__animated animate__lightSpeedInLeft animate__slow timeline-content'>
-                        <h3>2015-2016</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        A nulla animi error minus unde ex! Quidem assumenda ut alias
-                        nulla aperiam a nam placeat, ipsa reprehenderit sequi dolores
-                        ullam iusto.
-                        </p>
+                        <h3>2018-2020</h3>
+                        <h4>Lead Receiver - Give and Go Prepared foods</h4>
+                        <ul>
+                            <li>Used ERP system to ship/receive materials for production between different facilities</li>
+                            <li>Monitored emails periodically and accommodated any requests for shipment to other facilities</li>
+                            <li>Effectively communicated any requirements or issues to the appropriate departments</li>
+                            <li>Recorded any discrepancies/damages, documented and filed the necessary reports</li>
+                            <li>Received materials according to SOP, verified temperature, expiry and LOT IDs</li>
+                            <li>Worked with drivers and colleagues of other facilities to keep lines running smoothly at all facilities</li>
+                            <li>Performed counts for shift demands and weekly demands, ordered materials from other facilities according to demand</li>
+                        </ul>
                     </div>
             </div>
 
             <div className='timeline-block timeline-block-right'>
                 <div className='marker'></div>
                     <div className='animate__animated animate__lightSpeedInRight animate__slow timeline-content'>
-                        <h3>2016-2017</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        A nulla animi error minus unde ex! Quidem assumenda ut alias
-                        nulla aperiam a nam placeat, ipsa reprehenderit sequi dolores
-                        ullam iusto.
-                        </p>
+                        <h3>2017-2018</h3>
+                        <h4>Dock Shipper/Receiver - Apps Transport</h4>
+                        <ul>
+                            <li>Operated equipment in a safe and efficient manner.</li>
+                            <li>Stripped and loaded trailers in a cross-dock environment.</li>
+                            <li>Identified, dimmed and tagged freight appropriately</li>
+                            <li>Recorded any discrepancies/damages, documented and filed the necessary reports.</li>
+                            <li>Loaded trailers with attention to proper weight distribution and stability</li>
+                            <li>Handled dangerous goods</li>
+                            <li>Met or exceeded daily productivity quota</li>
+                        </ul>
                     </div>
             </div>
-
+{/* 
             <div className='timeline-block timeline-block-left'>
                 <div className='marker'></div>
                     <div className='animate__animated animate__lightSpeedInLeft animate__slow timeline-content'>
@@ -65,7 +88,7 @@ const Experience = () => {
                         ullam iusto.
                         </p>
                     </div>
-            </div>
+            </div> */}
         </div>
     </div>
   )
