63e31fab1df830ef39e37d963e1f19a8f11956f7
vithursan <93847590+Vithursan6@users.noreply.github.com>
2022-11-04 18:02:22 -0400

    updated about me section info

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,9 +13,11 @@ const AboutMe = () => {
             <div className="col-lg-6 col-xm-12">
                 <h1 className='about-heading'>About Me</h1>
                 <p className='about-p'>
-                    Hello! My Name is Vithursan Muthulingam, a recent graduate from University of Toronto's School of Continuing
-                    Studies Web Development Boot Camp. I have had a strong passion for computers and coding since I was a
-                    child and have been enrolled in several programming and animation programs at a young age.
+                    Hello! My Name is Vithursan, a graduate from University of Toronto's Web Development Program. I've had a strong
+                    passion for computers and coding since I was a child and have been enrolled in several programming and animation programs
+                    at a young age. I am a diligent, detail-oriented Software Developer passionate about learning, researching, and writing 
+                    code and I relish the opportunity to take my programming skills and experience to a company where I can continue to learn 
+                    and grow as a full stack developer.
                 </p>
             </div>
         </div>
