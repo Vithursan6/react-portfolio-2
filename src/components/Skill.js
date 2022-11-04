6f40b256b7e33ea852af70cd61689a9db2a093d2
vithursan <93847590+Vithursan6@users.noreply.github.com>
2022-11-04 18:43:06 -0400

    Updates

--- src/components/Skill.js
+++
@@ -1,7 +0,0 @@
-import 'animate.css'
-
-function Skill({source, alt, title}) {
-    return <img className='animate__animated animate__flipInX animate__slow skill-icon' src={source} alt={alt} title={title} />
-}
-
-export default Skill
